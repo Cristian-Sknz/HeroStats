92c986eda6c4d374de3496cc5b035ede2fce8aa7
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2020-11-18 11:26:33 -0300

    [feature] Agora é possível pegar histórico de campeões

--- src/main/java/me/skiincraft/discord/herostats/commands/HistoryCommand.java
+++ src/main/java/me/skiincraft/discord/herostats/commands/HistoryCommand.java
@@ -8,83 +8,135 @@ import me.skiincraft.api.paladins.enums.Platform;
 import me.skiincraft.api.paladins.exceptions.MatchException;
 import me.skiincraft.api.paladins.exceptions.SearchException;
 import me.skiincraft.api.paladins.objects.SearchPlayer;
-import me.skiincraft.discord.core.configuration.LanguageManager;
+import me.skiincraft.discord.core.common.reactions.ReactionObject;
+import me.skiincraft.discord.core.common.reactions.Reactions;
+import me.skiincraft.discord.core.common.reactions.custom.ReactionPage;
 import me.skiincraft.discord.herostats.assets.Category;
 import me.skiincraft.discord.herostats.assets.PaladinsCommand;
-import me.skiincraft.discord.herostats.exceptions.UnavailableAPIException;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 
-import java.util.Arrays;
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class HistoryCommand extends PaladinsCommand {
 
     public HistoryCommand() {
-        super("history", Collections.singletonList("historico"), "history <player> [platform]");
+        super("history", Collections.singletonList("historico"), "history <player> [champion/platform]");
     }
 
     public Category category() {
         return Category.Match;
     }
 
+    private String[] replaceSpaceChamps(String[] string){
+        return String.join(" ", string)
+                .toLowerCase()
+                .replace("sha lin", "sha_lin")
+                .replace("bomb king", "bomb_king")
+                .replace("mal damba", "mal'damba")
+                .replace("maldamba", "mal'damba")
+                .replace("bk", "bomb_king")
+                .split(" ");
+    }
+
     @Override
     public void execute(User user, String[] args, TextChannel channel) {
-        if (args.length == 0){
-            reply("!h" + getUsage());
+        if (args.length == 0) {
+            reply("h!" + getUsage());
             return;
         }
+        String[] newArgs = replaceSpaceChamps(args);
         try {
-            SearchPlayer searchPlayer = (args.length >= 2)
-                    ? searchPlayer(args[0], Platform.getPlatformByName(args[1]))
-                    : searchPlayer(args[0], Platform.PC);
+            SearchPlayer searchPlayer = (newArgs.length >= 2)
+                    ? searchPlayer(newArgs[0], (newArgs.length >= 3) ? Platform.getPlatformByName(newArgs[2]) : Platform.getPlatformByName(newArgs[1]))
+                    : searchPlayer(newArgs[0], Platform.PC);
 
             if (searchPlayer.isPrivacyFlag()) {
-                reply("Esse jogador se encontra com o perfil privado.");
+                reply(TypeEmbed.privateProfile().build());
                 return;
             }
 
             reply(TypeEmbed.processing().build(), message -> {
                 List<HistoryMatch> history = endpont().getMatchHistory(searchPlayer.getUserId()).get();
 
+                if (newArgs.length >= 3){
+                    Champion champ = champions(Language.Portuguese)
+                            .getAsStream()
+                            .filter(o -> o.getName().equalsIgnoreCase(newArgs[1].replace("_", " ")))
+                            .findAny()
+                            .orElse(null);
+                    if (champ != null){
+                        history = history.stream().filter(c -> c.getMatchPlayer().getChampionId() == champ.getId()).collect(Collectors.toList());
+                        if (history.size() == 0){
+                            message.editMessage(TypeEmbed.DefaultEmbed("Não foi possivel encontrar nenhuma partida", "Não consegui achar este campeão no historico.").build()).queue();
+                            return;
+                        }
+                    }
+                }
+
+                List<EmbedBuilder> embeds = new ArrayList<>();
+                int i = 1;
+                for (HistoryMatch match : history) {
+                    embeds.add(embed(match, i, history.size()));
+                    i++;
+                }
+
+                message.editMessage(embeds.get(0).build()).queue();
+                Objects.requireNonNull(Reactions.getInstance()).registerReaction(new ReactionObject(message, user.getIdLong(), new String[]{"U+25C0", "U+25B6"}),
+                        new ReactionPage(embeds, true));
             });
-        } catch (Exception e){
+        } catch (MatchException e) {
+            reply(TypeEmbed.DefaultEmbed("Não foi possivel encontrar nenhuma partida", "Algo aconteceu, e não foi possivel pegar as partidas").build());
+         } catch (SearchException e) {
+            reply(TypeEmbed.simpleEmbed(getLanguageManager().getString("Warnings", "T_INEXISTENT_USER"), getLanguageManager().getString("Warnings", "INEXISTENT_USER")).build());
+        } catch (Exception e) {
             reply(TypeEmbed.errorMessage(e, channel).build());
         }
     }
 
-    public EmbedBuilder embed(HistoryMatch match, SearchPlayer player){
+    public EmbedBuilder embed(HistoryMatch match, int ordem, int total) {
         EmbedBuilder embed = new EmbedBuilder();
         MatchPlayer matchPlayer = match.getMatchPlayer();
-
-        LanguageManager lang = getLanguageManager();
 
         Champion principalChampion = matchPlayer.getChampion(Language.Portuguese).get();
 
         embed.setAuthor(matchPlayer.getName(), null, principalChampion.getIcon());
         embed.setTitle("Historico de Partida");
-        String description = stringWrap(
-                ":trophy: Resultado: " + winnerConvert(match.getWinner()),
-                ":id: Identificação: `" + match.getMatchId() + "`",
-                ":game_die: Modo de Jogo: " + ((match.isRanked()) ? "Competitivo" : "Casual"));
 
-        embed.addField("Estatistica:", stringWrap(
-                "KDA: " + matchPlayer.getKillsRaw() + "/",
-                ""
-        ), true);
-        embed.setDescription(description);
+        embed.setDescription(String.format(":trophy: Resultado: %s", winnerConvert(match.getWinner())))
+                .appendDescription(String.format("\n:id: Id da Partida: `%s`", match.getMatchId()))
+                .appendDescription(String.format("\n:game_die: Modo de Jogo: %s", match.getQueue().getName()))
+                .appendDescription(String.format("\n:clock9: Duração: %s Minuto(s)", match.getMatchMinutes()));
+
+        NumberFormat nf = NumberFormat.getInstance();
+
+        embed.addField("Dano", nf.format(matchPlayer.getDamageRaw()), true);
+        embed.addField("Dano Sofrido", nf.format(matchPlayer.getDamage().getDamageTaken()), true);
+        embed.addField("KDA", matchPlayer.getKillsRaw() + "/" + matchPlayer.getDeaths(), true);
+
+        embed.addField("Cura", nf.format(matchPlayer.getHealing()), true);
+        embed.addField("Mapa", match.getMapGame(), true);
+        embed.addBlankField(true);
+
+        embed.setThumbnail(principalChampion.getIcon());
+        embed.setImage("https://i.imgur.com/hnKkxGR.png");
+
+        embed.setTimestamp(match.getMatchDate());
+        embed.setFooter(String.format("[%s/%s] Partida ocorreu em ", ordem, total));
+        embed.setColor(new Color(167, 255, 108));
 
         return embed;
     }
 
-    public String winnerConvert(String string){
+    public String winnerConvert(String string) {
         return (string.equalsIgnoreCase("Win")) ? "Vitória" : "Derrota";
-    }
-
-    public String stringWrap(String... string){
-        return String.join(" \n", string);
     }
 
 }
